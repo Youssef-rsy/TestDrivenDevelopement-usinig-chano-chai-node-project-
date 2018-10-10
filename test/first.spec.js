4786906474c471575ac60866e63131138b97cecc
ROSSAMYYOUSSEF <youssef.rsy@gmail.com>
2018-10-10 16:53:50 +0000

    first test

--- test/first.spec.js
+++ test/first.spec.js
@@ -2,12 +2,16 @@ var chai = require('chai');
 var assert = chai.assert;    // Using Assert style
 var expect = chai.expect;    // Using Expect style
 var should = chai.should();  
-
+function getTypeOfNumber(number){
+return "paire";
+}
   describe('#define if a number is paire or impaire ', function() {
     it('paire impaire number', function() {
         true.should.be.true;
     });
-    it('#if 0 then its a paire number');//  typeOf(0).should.equal(paire);
+    it('#if 0 then its a paire number',()=>{
+        getTypeOfNumber(0).should.equal("paire");
+    });//  typeOf(0).should.equal(paire);
     it('#if 1 then its a impaire number');
     it('#if 2 then its a paire number');
     it('#if 3 then its a impaire number');
